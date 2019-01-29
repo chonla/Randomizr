4cf3a2a50e58af44f0a248367465f29bf6c2da0b
Chonlasith Jucksriporn <chonlasith@gmail.com>
2019-01-29 11:59:34 +0700

    Update version

--- tests/Randomizr/RandomizrTest.php
+++ tests/Randomizr/RandomizrTest.php
@@ -1,46 +1,67 @@
 <?php
+declare(strict_types=1);
 
+use PHPUnit\Framework\TestCase;
 use Chonla\Randomizr\Randomizr;
-use \Mockery as m;
 
-class RandomizrTest extends PHPUnit_Framework_TestCase {
-    public function tearDown() {
-        m::close();
-    }
-
-    public function testRandShouldReturnStringWithGivenLength() {
+final class RandomizrTest extends TestCase {
+    public function testRandShouldReturnStringWithGivenLength(): void {
         $rand = new Randomizr;
         $return = $rand->rand('a', 9);
         $this->assertEquals(9, strlen($return));
     }
 
-    public function testRandShouldReturnOnlyCharacterInDomain() {
+    public function testRandShouldReturnOnlyCharacterInDomain(): void {
         $rand = new Randomizr;
         $return = $rand->rand('a', 9);
         $this->assertEquals('aaaaaaaaa', $return);
     }
 
-    public function testRandomNumberShouldPassNumberDomain() {
-        $rand = m::mock('Chonla\Randomizr\Randomizr[rand]');
-        $rand->shouldReceive('rand')->times(1)->with(Randomizr::NUMERIC, 4);
+    public function testRandomNumberShouldPassNumberDomain(): void {
+        $rand = $this->getMockBuilder(Chonla\Randomizr\Randomizr::class)
+                     ->setMethods(['rand'])
+                     ->getMock();
+
+        $rand->expects($this->once())
+             ->method('rand')
+             ->with(Randomizr::NUMERIC, 4);
+
         $rand->number(4);
     }
 
-    public function testRandomAlphaShouldPassAToZDomain() {
-        $rand = m::mock('Chonla\Randomizr\Randomizr[rand]');
-        $rand->shouldReceive('rand')->times(1)->with(Randomizr::ALPHABETIC, 9);
+    public function testRandomAlphaShouldPassAToZDomain(): void {
+        $rand = $this->getMockBuilder(Chonla\Randomizr\Randomizr::class)
+                     ->setMethods(['rand'])
+                     ->getMock();
+
+        $rand->expects($this->once())
+            ->method('rand')
+            ->with(Randomizr::ALPHABETIC, 9);
+        
         $rand->alphabet(9);
     }
 
-    public function testRandomAlphaNumericShouldPassAlphaNumericDomain() {
-        $rand = m::mock('Chonla\Randomizr\Randomizr[rand]');
-        $rand->shouldReceive('rand')->times(1)->with(Randomizr::ALPHANUMERIC, 7);
+    public function testRandomAlphaNumericShouldPassAlphaNumericDomain(): void {
+        $rand = $this->getMockBuilder(Chonla\Randomizr\Randomizr::class)
+                     ->setMethods(['rand'])
+                     ->getMock();
+
+        $rand->expects($this->once())
+            ->method('rand')
+            ->with(Randomizr::ALPHANUMERIC, 7);
+
         $rand->alphanumeric(7);
     }
 
-    public function testRandomHexadecimalShouldPassHexadecimalcDomain() {
-        $rand = m::mock('Chonla\Randomizr\Randomizr[rand]');
-        $rand->shouldReceive('rand')->times(1)->with(Randomizr::HEXADECIMAL, 7);
+    public function testRandomHexadecimalShouldPassHexadecimalcDomain(): void {
+        $rand = $this->getMockBuilder(Chonla\Randomizr\Randomizr::class)
+                     ->setMethods(['rand'])
+                     ->getMock();
+
+        $rand->expects($this->once())
+            ->method('rand')
+            ->with(Randomizr::HEXADECIMAL, 7);
+
         $rand->hexadecimal(7);
     }
 }
